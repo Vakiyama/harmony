b2ee69b3468a6a9fad38501327fe46ddeec9673c
Vakiyama <vakiyama@outlook.com>
2024-10-05 00:14:41 -0700

    fix: remove nm

--- node_modules/@libsql/isomorphic-fetch/node.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const _Request = Request;
-const _Headers = Headers;
-
-const _fetch = fetch;
-
-module.exports = { fetch: _fetch, Request: _Request, Headers: _Headers };
--- node_modules/@libsql/isomorphic-fetch/node.js
+++
@@ -1,6 +0,0 @@
-const _Request = Request;
-const _Headers = Headers;
-
-const _fetch = fetch;
-
-export { _fetch as fetch, _Request as Request, _Headers as Headers};
