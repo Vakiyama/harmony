92d09b32ce538c510aeb23e79e376226630e47a5
Angus Ng <ang183@my.bcit.ca>
2024-10-17 02:55:51 -0700

    implement headless oauth and email login

--- solid-app/src/routes/api/auth/login.tsx
+++ solid-app/src/routes/api/auth/login.tsx
@@ -1,49 +1,39 @@
 import { Button } from "~/components/ui/button";
-import { A } from "@solidjs/router";
 import MediumLogo from "~/components/svg/mediumLogo";
 import { TextField, TextFieldRoot } from "~/components/ui/textfield";
+import { emailLogin } from "~/api/auth-server-actions";
 
 export default function Login() {
   return (
     <main class="absolute top-[3rem] w-full h-[calc(100%-3rem)] p-4 flex items-center justify-center overflow-hidden">
-      <div class="mt-8 mb-8 flex flex-col items-center justify-center max-w-md">
-        <div class="mt-8 mb-8 grid grid-rows-3 w-full">
-          <div class="h-12 mb-4"></div>
-        </div>
+      <div class="mt-64 mb-6 flex flex-col items-center justify-center max-w-md">
         <MediumLogo class="mt-8 mb-8" />
-        <div class="mb-4">
+        <div class="mb-8">
           <h1 class="text-[28px] normal-case text-gray-600">Welcome back!</h1>
         </div>
-        <div>
-          <TextFieldRoot>
-            <TextField
-              class="h-12 w-[368px] rounded-[100px] mb-4"
-              type="email"
-              placeholder="Email"
-              name="email"
-            />
-          </TextFieldRoot>
-          <TextFieldRoot>
-            <TextField
-              class="h-12 w-[368px] rounded-[100px] mb-4"
-              type="password"
-              placeholder="Password"
-              name="password"
-            />
-          </TextFieldRoot>
-        </div>
-        <div class="mt-8 w-full">
-          <a href="/api/auth/login" class="mx-auto">
+        <form action={emailLogin} class="appearance-none" method="post">
+          <div class="mb-8">
+            <TextFieldRoot>
+              <TextField
+                class="h-12 w-[368px] rounded-[100px] mb-4"
+                type="email"
+                placeholder="Email"
+                name="email"
+              />
+            </TextFieldRoot>
+          </div>
+          <div class="mt-4 w-full">
             <Button
               class="rounded-[100px] h-12 w-[368px] mb-4"
               variant="default"
+              type="submit"
             >
               Login
             </Button>
-          </a>
-        </div>
+          </div>
+        </form>
         <div class="w-full">
-          <a href="/" class="m-0 text-[13px]">
+          <a href="/api/auth/reset-password" class="m-0 text-[13px]">
             Forgot your Password?
           </a>
         </div>
