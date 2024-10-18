7930533b03c08f4e93fd5678639b7ec0b9a3571f
Angus Ng <ang183@my.bcit.ca>
2024-10-18 15:27:29 -0700

    update server file so auth works on branch after user table rename

--- solid-app/src/api/server.ts
+++ solid-app/src/api/server.ts
@@ -1,28 +1,28 @@
-'use server';
-import { redirect } from '@solidjs/router';
-import { eq } from 'drizzle-orm';
-import { db } from './db';
-import { getKindeClient, sessionManager } from './kinde';
-import { UserType } from '@kinde-oss/kinde-typescript-sdk';
-import { Users } from '../../drizzle/schema/users';
+"use server";
+import { redirect } from "@solidjs/router";
+import { eq } from "drizzle-orm";
+import { db } from "./db";
+import { getKindeClient, sessionManager } from "./kinde";
+import { UserType } from "@kinde-oss/kinde-typescript-sdk";
+import { users } from "../../drizzle/schema/users";
 
 function validateUsername(username: unknown) {
-  if (typeof username !== 'string' || username.length < 3) {
+  if (typeof username !== "string" || username.length < 3) {
     return `Usernames must be at least 3 characters long`;
   }
 }
 
 function validatePassword(password: unknown) {
-  if (typeof password !== 'string' || password.length < 6) {
+  if (typeof password !== "string" || password.length < 6) {
     return `Passwords must be at least 6 characters long`;
   }
 }
 
-async function login(kindeUser : UserType) {
+async function login(kindeUser: UserType) {
   const user = await db
     .select()
-    .from(Users)
-    .where(eq(Users.kindeId, kindeUser.id))
+    .from(users)
+    .where(eq(users.kindeId, kindeUser.id))
     .get();
   return user;
 }
@@ -30,26 +30,30 @@ async function login(kindeUser : UserType) {
 async function register(kindeUser: UserType) {
   const existingUser = await db
     .select()
-    .from(Users)
-    .where(eq(Users.kindeId, kindeUser.id))
+    .from(users)
+    .where(eq(users.kindeId, kindeUser.id))
     .get();
-  if (existingUser) throw new Error('User already exists');
-  return db.insert(Users).values({
-     kindeId: kindeUser.id, 
-     displayName: kindeUser.given_name,
-     firstName: kindeUser.given_name,
-     lastName: kindeUser.family_name,
-     photo: kindeUser.picture!,
-     email: kindeUser.email,
-     roleType: "other"
-    }).returning().get();
+  if (existingUser) throw new Error("User already exists");
+  return db
+    .insert(users)
+    .values({
+      kindeId: kindeUser.id,
+      displayName: kindeUser.given_name,
+      firstName: kindeUser.given_name,
+      lastName: kindeUser.family_name,
+      photo: kindeUser.picture!,
+      email: kindeUser.email,
+      roleType: "other",
+    })
+    .returning()
+    .get();
 }
 
 export async function loginOrRegister(kindeUser: UserType) {
   try {
-    let user = await login(kindeUser)
+    let user = await login(kindeUser);
     if (!user) {
-      user = await register(kindeUser)
+      user = await register(kindeUser);
     }
     const session = (await sessionManager()).getSession();
     await session.update((d) => {
@@ -74,23 +78,23 @@ export async function getUser() {
   try {
     const user = await db
       .select()
-      .from(Users)
-      .where(eq(Users.id, userId))
+      .from(users)
+      .where(eq(users.id, userId))
       .get();
-    if (!user) throw redirect('/api/auth/login');
+    if (!user) throw redirect("/api/auth/login");
     return { id: user.id, displayName: user.displayName };
   } catch {
     throw logout();
   }
 }
 
-export async function checkAuthenticated () {
-  const manager = await sessionManager()
-  const isAuthenticated = await getKindeClient().isAuthenticated(manager)
+export async function checkAuthenticated() {
+  const manager = await sessionManager();
+  const isAuthenticated = await getKindeClient().isAuthenticated(manager);
 
   if (!isAuthenticated) {
-    throw redirect('/api/auth/login')
+    throw redirect("/api/auth/login");
   }
-  
+
   return manager;
 }
