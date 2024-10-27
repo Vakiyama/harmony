634f05072a9b1895bce26b73b8038dc34ca8a706
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-10-26 18:56:12 -0700

    feat: add coming up part

--- solid-app/src/components/landing/LandingHeader.tsx
+++ solid-app/src/components/landing/LandingHeader.tsx
@@ -1,8 +1,33 @@
+import { Title } from "@solidjs/meta";
+import { BiSolidBell } from "solid-icons/bi";
+import { FaSolidAngleDown } from "solid-icons/fa";
+
 export function LandingHeader() {
+  const currentDate = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    // year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
-    <div>
-      <h1>Lola's Care Circle</h1>
-      <p>Hello</p>
+    <div class="flex justify-between items-center p-4 bg-white shadow-md">
+      <Title class="text-3xl">Lola's Care Circle</Title>
+
+      <div class="flex items-center">
+        <div class="flex flex-col">
+          <h1 class="text-2xl font-semibold mr-2">Lola's Care Circle</h1>
+
+          <p class="text-sm text-gray-500">{currentDate}</p>
+        </div>
+        <div class="p-2">
+          <FaSolidAngleDown class="text-gray-600" size={20} />
+        </div>
+      </div>
+
+      <div class="flex flex-col items-end">
+        <BiSolidBell class="text-black-600 text-xl mb-2" size={27} />
+      </div>
     </div>
   );
 }
