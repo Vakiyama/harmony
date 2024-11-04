a0c299f9b97f58ce3f660636739b3f3ba9dc72ea
Max <maxli88488@gmail.com>
2024-11-03 18:18:12 -0800

    add placeholder location

---
+++ solid-app/src/components/calendar/calendar-detail-top-nav.tsx
@@ -0,0 +1,22 @@
+import { A } from "@solidjs/router";
+
+export default function EventDetailsTopNav() {
+  return (
+    <div class="sticky top-0 w-full flex flex-row h-[95px] bg-[#fcfcfc] shadow-md border-b border-[#1e1e1e]/20">
+      <div class="w-full flex flex-row justify-between items-end px-4 mb-4">
+        <A
+          class="text-[#1e1e1e] text-base font-normal font-sf-pro leading-tight"
+          href="/calendar"
+        >
+          Back
+        </A>
+        <div class="text-[#1e1e1e] text-[19px] font-medium font-grotesque leading-[22.80px]">
+          Event Details
+        </div>
+        <button class="text-[#1e1e1e] text-base font-normal font-sf-pro leading-tight">
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+}
