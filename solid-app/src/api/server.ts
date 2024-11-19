7b44ca661a0be6497a56c3d87baaa057e4d55873
Vakiyama <vakiyama@outlook.com>
2024-11-18 23:18:52 -0800

    fix: ensure user exists before seeding

--- solid-app/src/api/server.ts
+++ solid-app/src/api/server.ts
@@ -66,7 +66,7 @@ export async function loginOrRegister(kindeUser: UserTypeExtended) {
     if (!user) {
       user = await register(kindeUser);
     }
-    await seedData(user);
+    user && await seedData(user);
     const session = (await sessionManager()).getSession();
     await session.update((d) => {
       d.userId = user.id;
