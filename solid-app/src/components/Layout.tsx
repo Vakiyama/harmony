4e177f4436abed92e297e1b8df4584c7179950ae
Max <maxli88488@gmail.com>
2024-11-22 03:07:42 -0800

    dont show nav bar when on calendar event detail page

--- solid-app/src/components/Layout.tsx
+++ solid-app/src/components/Layout.tsx
@@ -115,7 +115,10 @@ const Layout: Component<{ children: JSXElement }> = (props) => {
         <div class="flex-none max-h-[77px] bg-transparent">
           {location.pathname.startsWith("/api") ||
           location.pathname.startsWith("/harmony-ai") ||
-          location.pathname.startsWith("/team/create") ? null : (
+          location.pathname.startsWith("/team/create") ||
+          location.pathname.startsWith(
+            `/team/${params.id}/calendar/${params.eventId}`
+          ) ? null : (
             <NavBar teamData={teamListData()} />
           )}
         </div>
