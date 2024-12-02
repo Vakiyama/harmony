50d61fe383eaeb572b5ee15daae39c510461605b
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-12-01 21:58:17 -0800

    fix: resolve merge conflict with remote branch

--- solid-app/src/components/profile/team-card.tsx
+++ solid-app/src/components/profile/team-card.tsx
@@ -10,17 +10,14 @@ export default function TeamCard(props: {
   return (
     <A
       href={props.href}
-      // class="flex flex-col items-center border border-gray-300 rounded-md min-w-[177px] min-h-[196px] w-full h-full"
-      class="flex flex-col items-center border border-gray-300 rounded-md w-full h-full aspect-[10/9.8]"
+      class="flex flex-col items-center border border-gray-300 rounded-md min-w-[177px] min-h-[196px] w-full h-full"
     >
-      <div class="flex flex-col w-full h-full">
-        <div class="">
-          <TeamImage teamName={props.teamName} imageUrl={props.imageUrl} />
-        </div>
+      <div class="flex flex-grow">
+        <TeamImage teamName={props.teamName} imageUrl={props.imageUrl} />
+      </div>
 
-        <div class="flex items-center justify-center text-base font-medium w-full h-[35px] overflow-hidden">
-          {props.teamName}
-        </div>
+      <div class="flex items-center justify-center text-base font-medium w-full h-[35px] overflow-hidden">
+        {props.teamName}
       </div>
     </A>
   );
