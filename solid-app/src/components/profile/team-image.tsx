2a3e9af0d7132f3e75ae4fb1e809935a265cfa24
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-12-01 19:32:52 -0800

    fix: consistent team image sizes

--- solid-app/src/components/profile/team-image.tsx
+++ solid-app/src/components/profile/team-image.tsx
@@ -9,9 +9,13 @@ export default function TeamImage(props: {
 }) {
   return (
     <>
-      <div class="">
+      <div class="h-[161px] w-full overflow-hidden rounded-t-md">
         <ImageRoot class="items-center justify-center h-full w-full object-cover">
-          <Image src={props.imageUrl} alt={props.teamName} class="" />
+          <Image
+            src={props.imageUrl}
+            alt={props.teamName}
+            class="object-cover w-full h-full"
+          />
           <ImageFallback class="font-semi items-center justify-center bg-purple-200"></ImageFallback>
         </ImageRoot>
       </div>
