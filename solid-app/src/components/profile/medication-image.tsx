f03546e5723a4f1ab9de02c036bd0fb46b723458
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-12-03 20:32:25 -0800

    feat: show medication image in profile

--- solid-app/src/components/profile/medication-image.tsx
+++ solid-app/src/components/profile/medication-image.tsx
@@ -1,13 +1,13 @@
 import { Image, ImageFallback, ImageRoot } from "~/components/ui/image";
 export default function MedicationImage(props: { pharmacyImg: string | "" }) {
   return (
-    <ImageRoot class="flex items-center justify-center h-[234px] w-full bg-gray-200  rounded-lg border overflow-hidden">
+    <ImageRoot class="flex items-center justify-center h-[234px] w-full bg-white-200  rounded-lg border overflow-hidden">
       <Image
         src={props.pharmacyImg}
         alt="hngngn"
         class="object-contain w-full h-full"
       />
-      <ImageFallback class="flex items-center justify-center w-full h-full bg-gray-200 rounded-lg">
+      <ImageFallback class="flex items-center justify-center w-full h-full bg-white-200 rounded-lg">
         Not available
       </ImageFallback>
     </ImageRoot>
