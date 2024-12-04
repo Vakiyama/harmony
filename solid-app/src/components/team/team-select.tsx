613d4ed7b69220af093d1720734e8acd865735e7
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-12-03 21:28:14 -0800

    fix: update gender text color to black when selected, gray when not selected

--- solid-app/src/components/team/team-select.tsx
+++ solid-app/src/components/team/team-select.tsx
@@ -32,7 +32,9 @@ export const SelectTrigger = <T extends ValidComponent = "button">(
   return (
     <SelectPrimitive.Trigger
       class={cn(
-        "flex h-9 w-full items-center justify-between rounded-md border border-gray-200 bg-transparent px-3 py-2 shadow-sm ring-offset-background transition-shadow placeholder:text-muted-foreground focus:outline-none focus-visible:ring-[1.5px] focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-gray-500",
+        "flex h-9 w-full items-center justify-between rounded-md border border-gray-200 bg-transparent px-3 py-2 shadow-sm ring-offset-background transition-shadow placeholder:text-muted-foreground focus:outline-none focus-visible:ring-[1.5px] focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "data-[selected]:text-black",
+
         local.class
       )}
       {...rest}
@@ -71,12 +73,12 @@ export const SelectContent = <T extends ValidComponent = "div">(
     <SelectPrimitive.Portal>
       <SelectPrimitive.Content
         class={cn(
-          "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0 data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95",
+          " z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0 data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95",
           local.class
         )}
         {...rest}
       >
-        <SelectPrimitive.Listbox class="p-1 focus-visible:outline-none" />
+        <SelectPrimitive.Listbox class="p-1 focus-visible:outline-none text-black" />
       </SelectPrimitive.Content>
     </SelectPrimitive.Portal>
   );
@@ -98,11 +100,12 @@ export const SelectItem = <T extends ValidComponent = "li">(
     <SelectPrimitive.Item
       class={cn(
         "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        "data-[selected]:!text-black",
         local.class
       )}
       {...rest}
     >
-      <SelectPrimitive.ItemIndicator class="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
+      <SelectPrimitive.ItemIndicator class="absolute right-2 flex h-3.5 w-3.5 items-center justify-center ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-4 w-4"
@@ -119,7 +122,11 @@ export const SelectItem = <T extends ValidComponent = "li">(
           <title>Checked</title>
         </svg>
       </SelectPrimitive.ItemIndicator>
-      <SelectPrimitive.ItemLabel>{local.children}</SelectPrimitive.ItemLabel>
+      <SelectPrimitive.ItemLabel
+        class={cn("data-[selected]:!text-black", "text-gray-500")}
+      >
+        {local.children}
+      </SelectPrimitive.ItemLabel>
     </SelectPrimitive.Item>
   );
 };
